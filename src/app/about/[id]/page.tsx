d0a158721626bae54c916b18f0e6ef967ed4a92e
Maximka <benko_0287.mail.ru>
2025-01-03 23:12:48 +0300

    122222

--- src/app/about/[id]/page.tsx
+++ src/app/about/[id]/page.tsx
@@ -1,7 +1,6 @@
 import styles from "./styles.module.css";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import Link from "next/link";
-import { IPoste } from "../page";
 
 type Props = {
   params: {
@@ -9,15 +8,13 @@ type Props = {
   };
 };
 
-export async function generateMetadata({
-  params: { id },
-}: Props): Promise<Metadata> {
-  return {
-    title: id,
-  };
-}
+// export async function generateMetadata({ params: { id } }: Props) {
+//   return {
+//     title: id,
+//   };
+// }
 
-const getData = async ({ params: { id } }: Props): Promise<IPoste> => {
+const getData = async (id: string) => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}`
   );
@@ -25,7 +22,7 @@ const getData = async ({ params: { id } }: Props): Promise<IPoste> => {
 };
 
 export default async function id({ params: { id } }: Props) {
-  const response = await getData({ params: { id } });
+  const response = await getData(id);
 
   return (
     <div className={styles.container}>
